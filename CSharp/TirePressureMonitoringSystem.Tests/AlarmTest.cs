2584a5cb6c676b702ad8355b083579e4b7ad72df
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-10-19 17:11:10 +1300

    Add threshold tests

--- CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
+++ CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -1,14 +1,44 @@
+using System;
 using Xunit;
 
 namespace TDDMicroExercises.TirePressureMonitoringSystem
 {
+    public class TestSensor : Sensor
+    {
+        private readonly double _pressure;
+
+        public TestSensor(double pressure)
+        {
+            _pressure = pressure;
+        }
+        
+        public override double PopNextPressurePsiValue() => _pressure;
+    }
+    
     public class AlarmTest
     {
-        [Fact]
-        public void Foo()
+        [Theory]
+        [InlineData(17)]
+        [InlineData(19.1)]
+        [InlineData(21)]
+        public void WhenThePressureIsWithinLimits_ThenTheAlarmIsNotOn(double pressure)
         {
-            Alarm alarm = new Alarm();
+            var alarm = new Alarm(new TestSensor(pressure));
+            alarm.Check();
             Assert.False(alarm.AlarmOn);
         }
+        
+        [Theory]
+        [InlineData(double.MinValue)]
+        [InlineData(16.999999999999)]
+        [InlineData(21.000000000001)]
+        [InlineData(double.MaxValue)]
+        public void WhenThePressureIsOutsideLimits_ThenTheAlarmIsOn(double pressure)
+        {
+            var alarm = new Alarm(new TestSensor(pressure));
+            alarm.Check();
+            Assert.True(alarm.AlarmOn);
+        }
+        
     }
 }
